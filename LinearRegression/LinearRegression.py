1352cf0d45f5c32a4aabfbb7b9ffc3abc30335cb
Rodolfo B. Martini <rodolfo.martini3@gmail.com>
2017-01-30 17:46:48 -0200

    Teste Git Push

--- LinearRegression/LinearRegression.py
+++ LinearRegression/LinearRegression.py
@@ -4,5 +4,10 @@ Created on Mon Jan 30 17:28:51 2017
 
 @author: Rodolfo Braga Martini
 """
-print(2+2)
+import pandas as pd
+import matplotlib as plt
+import sklearn as skt
+
+Data = skt.utils.__path__
+print(Data)
 
